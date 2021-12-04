eec43edd7efbe72118e481902145c69305ebb4d9
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-12-04 10:51:07 +0100

    Made Template

---
+++ template.py
@@ -0,0 +1,13 @@
+def ext1(input : list[str]) -> int:
+    pass
+
+def ext2(input : list[str]) -> int:
+    pass
+
+
+file = open("DayX/data.txt")
+
+lines = file.readlines()
+
+print(ext1(lines))
+print(ext2(lines))
