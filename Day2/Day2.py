69a67a75ed08ea5ea5fda425550cd2dc2aed6182
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-12-04 11:58:25 +0100

    Finished ex2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -14,7 +14,20 @@ def ex1(input : list[str]) -> int:
         
 
 def ex2(input : list[str]) -> int:
-    pass
+    posX = 0
+    depth = 0
+    aim = 0
+    for i in range(len(lines)):
+        currentLine = lines[i].split(" ")
+        if str(currentLine[0]) == "forward":
+            posX += int(currentLine[1])
+            depth += aim*int(currentLine[1])
+        if str(currentLine[0]) == "up":
+            aim -=int(currentLine[1])
+        if str(currentLine[0]) == "down":
+            aim +=int(currentLine[1])
+
+    return posX*depth
 
 
 file = open("Day2/data.txt")
