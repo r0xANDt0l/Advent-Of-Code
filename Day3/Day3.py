e24b593da6f203c62ed86e598d79cbca8515871a
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2021-12-11 11:51:26 +0100

    removed desc

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,25 +1,12 @@
 def ex1(input : list[str]) -> int:
-# The submarine has been making some odd creaking noises, so you ask it to produce a diagnostic report just in case.
-
-# The diagnostic report (your puzzle input) consists of a list of binary numbers which,
-# when decoded properly, can tell you many useful things about the conditions of the submarine.
-# The first parameter to check is the power consumption.
-
-# You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the epsilon rate).
-# The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
-# Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers
-# in the diagnostic report. For example, given the following diagnostic report:    
-    
-# Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together. 
-# What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
-    bitarray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    nCul = len(input[0]) -1
+    bitarray = [0 for i in range(nCul)]
     for i in range(len(lines)):
         for a in len[bitarray]:
             if str(lines[a]) == "0":
                 bitarray[a] -= 1
             else:
                 bitarray[a] += 1
-    return bitarray  
 
 
 def ex2(input : list[str]) -> int:
