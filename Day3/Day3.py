7ba4c34c58717cf786b78590ab879da2746491eb
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2021-12-18 11:32:57 +0100

    finished D3 ex2

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -19,15 +19,30 @@ def ex1(input : list[str]) -> int:
             e+="1"
     return int(g,2) * int(e,2)
 
-def discrim():
-    nCul = len(input[0]) -1
-    bitarray = [0 for i in range(nCul)]
-    for i in input:
-        for j in range(nCul):
-            if i[j] == "0":
-                bitarray[j] -= 1
+def discrim(input : list[str] , common: bool):
+    auxlist = input
+    retlist = []
+
+    col = 0
+    while len(retlist) !=1:
+        bitarray = 0
+        for i in auxlist:
+            if i[col] == "0":
+                bitarray -= 1
             else:
-                bitarray[j] += 1
+                bitarray += 1
+    
+        crit = "0" if (bitarray < 0 and common) or (bitarray >= 0 and not common) else "1"
+
+        retlist = []    
+        for i in auxlist:
+            if i[col] == crit:
+                retlist.append(i)
+
+        auxlist = retlist
+        col +=1
+
+    return retlist[0]
 
 def ex2(input : list[str])-> int:
 
@@ -38,7 +53,7 @@ def ex2(input : list[str])-> int:
     return int(O2,2) * int(CO2,2)
 
 
-file = open("Day3/dataTest.txt")
+file = open("Day3/data.txt")
 
 lines = file.readlines()
 
